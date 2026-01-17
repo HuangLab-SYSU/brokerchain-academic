b90639ec6dd98674675028da558e964969810cda
Yin <yzl1154695556@qq.com>
2026-01-17 16:43:12 +0800

    fix: 修复私钥进制读取问题

--- global/global.go
+++ global/global.go
@@ -12,7 +12,6 @@ var PrivateKeyBigInt *big.Int = new(big.Int)
 var Uint = "1000000000000000000"
 var LocalPort = 63259
 
-// var ServerHost = "127.0.0.1"
 var ServerHost = "dash.broker-chain.com"
 var ProxyServerHost = "dash.broker-chain.com"
 
